a7dd499c0b642133d99d4b5f2a76bdecbef64a43
Artur Dymkowski <arturdymkowski@wp.pl>
2026-07-06 13:57:49 +0200

    fix display driver name in driver form

--- resources/views/pages/drivers/edit.blade.php
+++ resources/views/pages/drivers/edit.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <x-common.page-breadcrumb
-        pageTitle="{{ $driver->name }}"
+        pageTitle="{!! $driver->name !!}"
         :breadcrumbs="[
         __('drivers.plural_model_label') => route('drivers.index'),
         __('drivers.singular_model_label') => route('drivers.edit', ['driver' => $driver]),
